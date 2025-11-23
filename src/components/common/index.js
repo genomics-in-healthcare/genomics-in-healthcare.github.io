0c0563568947df688083133dce32e2c25eef19dc
Tony Y <94228508+tonyy01@users.noreply.github.com>
2025-11-23 17:41:30 +0800

    Update package-lock.json and refactor component exports in index.js to unify UI component usage

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -5,5 +5,6 @@ export { default as NewsItem } from './NewsItem'
 export { default as MemberCard } from './MemberCard'
 export { default as LogoList } from './LogoList'
 
-
+// 重新导出UI组件以便统一使用
+export { Section, Container } from '../ui'
 
